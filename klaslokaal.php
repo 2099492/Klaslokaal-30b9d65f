e1be5acc6270f5f5ae5624973f56e11ecbfe8378
2099492 <2099492@talnet.com>
2019-11-20 13:40:42 +0100

    Uitwerking van de exercise

---
+++ klaslokaal.php
@@ -0,0 +1,11 @@
+<?php
+$a = array("naam", "naam", "naam");
+//echo $a[0 && 1 && 2] . PHP_EOL;
+//echo $a[1] . PHP_EOL;
+//echo $a[2] . PHP_EOL;
+
+foreach ($a as $key => $value) {
+  print($value . PHP_EOL);
+}
+
+ ?>
